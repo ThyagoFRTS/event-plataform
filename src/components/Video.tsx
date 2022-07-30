5a1eb313fd1f35eefaab8cce360ed764b3354508
ThyagoKZKR <kzkr.thyago@gmail.com>
2022-07-29 22:56:43 -0300

    fix nullable teacher div

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -47,7 +47,7 @@ const Video = (props: Props) => {
         console.log("on effect")
         console.log(data)
     }, [])
-    
+
     if (!data) {
         return (
             <div className="flex-1">
@@ -55,7 +55,7 @@ const Video = (props: Props) => {
             </div>
         )
     }
-    
+
 
 
     return (
@@ -77,20 +77,22 @@ const Video = (props: Props) => {
                         <p className="mt-4 text-gray-200 leading-relaxed">
                             {data.lesson.description}
                         </p>
-                        <div className="flex items-center gap-4 mt-6">
-                            <img
-                                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src={data.lesson.teacher.avatarURL}
-                                alt="Profile Picture" />
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block" >
-                                    {data.lesson.teacher.name}
-                                </strong>
-                                <span className="tex-gray-200 text-sm block">
-                                    {data.lesson.teacher.bio}
-                                </span>
-                            </div>
-                        </div>
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
+                                <img
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                    src={data.lesson.teacher.avatarURL}
+                                    alt="Profile Picture" />
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block" >
+                                        {data.lesson.teacher.name}
+                                    </strong>
+                                    <span className="tex-gray-200 text-sm block">
+                                        {data.lesson.teacher.bio}
+                                    </span>
+                                </div>
+                            </div>)
+                        }
                     </div>
                     <div className="flex flex-col gap-4">
                         <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
